309218b0eccb76358d4f8da062f064f3c2192bb9
Andrew <satori.akarin@gmail.com>
2017-12-15 21:54:33 +0200

    feat: add main notes from  You Don't Know JS: Up & Going (start - blocks) and freecodecamp (1-80)

--- Study/strings.js
+++ Study/strings.js
@@ -1,11 +1,9 @@
 var myName;
 myName = "My name is " + myName + " and I am swell!";
 
-
 var someAdjective = "...";
 var myStr = "Learning to code is ";
 myStr += someAdjective;
-
 
 var firstLetterOfLastName = "";
 var lastName = "Lovelace";
@@ -29,6 +27,21 @@ var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 var myData = myArray[2][1]; // = 8
 var myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]); //add to array
-var removedFromMyArray = myArray.pop();  //take last item from array, 
+var removedFromMyArray = myArray.pop();  //delete last element from myArray, and = it to removedFromMyArray
 //now removedFromMyArray === ["cat", 2] and  myArray === [["John", 23]]
-var removedFromMyArray = myArray.shift(); //take first item from array, 
+var removedFromMyArray = myArray.shift(); //delete first element from myArray, and = it to removedFromMyArray
+myArray.unshift(["Paul", 35]); //create a element at start of array
+console.log(amount);				// 215.9784
+console.log(amount.toFixed(2));	// "215.98"
+
+var SOME_THING = 21; //Constant
+const NEXT_THING = 21; //Constant in ES6
+
+// variable within a function without var === global variable  global = 5;
+
+function timesFive(num) {  //return number multiplied by 5
+  return num * 5;
+}
+result = timesfive(3);
+
+
