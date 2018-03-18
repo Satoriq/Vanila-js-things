d992258423322130fac087263946b7c46407ca56
Andrew <sudo.satoriq@gmail.com>
2018-03-18 23:55:45 +0200

    add: OOP(proto)

--- Study/notes.js
+++ Study/notes.js
@@ -2608,7 +2608,67 @@ var rabbit = new Rabbit('Rabit');
 rabbit.run();
 
 
+//instanceof для классов
+obj instanceof Constructor
 
+function Rabbit() {}
+var rabbit = new Rabbit();
+// проверяем -- этот объект создан Rabbit?
+alert( rabbit instanceof Rabbit ); // true
+
+
+// Создание своих ошибок
+
+// объявление
+function PropertyError(property) {
+  this.name = "PropertyError";
+
+  this.property = property;
+  this.message = "Ошибка в свойстве " + property;
+
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(this, PropertyError); 
+    // для дальнейшего наследования изменим на
+    Error.captureStackTrace(this, this.constructor);
+  } else {
+    this.stack = (new Error()).stack;
+  }
+
+}
+PropertyError.prototype = Object.create(Error.prototype);
+// для дальнейшего наследования добавим
+PropertyError.prototype.constructor = PropertyError;
+
+
+// генерация ошибки
+function readUser(data) {
+  var user = JSON.parse(data);
+  if (!user.age) {
+    throw new PropertyError("age");
+  }
+  if (!user.name) {
+    throw new PropertyError("name");
+  }
+  return user;
+}
+
+// запуск и try..catch
+try {
+  var user = readUser('{ "age": 25 }');
+} catch (err) {
+  if (err instanceof PropertyError) {
+    if (err.property == 'name') {
+      // если в данном месте кода возможны анонимы, то всё нормально
+      alert( "Здравствуйте, Аноним!" );
+    } else {
+      alert( err.message ); // Ошибка в свойстве ...
+    }
+  } else if (err instanceof SyntaxError) {
+    alert( "Ошибка в синтаксисе данных: " + err.message );
+  } else {
+    throw err; // неизвестная ошибка, не знаю что с ней делать
+  }
+}
 
 
 
